afa017cc081de8c7b9e3155a8628dc68e264b34a
VitorAngelozi <vitorangelozi2013@gmail.com>
2025-12-10 18:51:54 -0300

    canal com buffer

---
+++ canal_com_buffer/canal_com_buffer.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	canal := make(chan int, 3) // canal com capacidade para 3 valores
+
+	canal <- 1 // imediato
+	canal <- 2 // imediato
+	canal <- 3 // imediato — ainda há espaço
+
+	// Se eu tentasse canal <- 4 aqui, travaria! (canal cheio)
+
+	fmt.Println(<-canal) // lê o primeiro valor
+	fmt.Println(<-canal) // lê o segundo valor
+	fmt.Println(<-canal) // lê o terceiro valor
+
+	canal <- 4
+	canal <- 5
+	canal <- 6
+	fmt.Println(<-canal) // lê o terceiro valor
+	fmt.Println(<-canal) // lê o terceiro valor
+	fmt.Println(<-canal) // lê o terceiro valor
+}
